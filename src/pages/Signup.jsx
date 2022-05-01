d7b93f9cd049579f4be28df6b05488f8d5a7128d
saintrabby <saintrabby@gmail.com>
2022-05-02 03:56:15 +0900

    chat_test
    testing

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,33 +1,8 @@
 import React from 'react'
-import { useForm } from "react-hook-form";
-
-import "./styles.css";
 
 const Signup = () => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors }
-      } = useForm();
-      const onSubmit = (data) => {
-        console.log(data);
-      }; // your form submit function which will invoke after successful validation
-    
-      console.log(watch("example")); // you can watch individual input by pass the name of the input
-    
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
-  
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <p>This field is required</p>}
-  
-        <input type="submit" />
-      </form>
+        <p>Signup</p>
     )
 }
 
