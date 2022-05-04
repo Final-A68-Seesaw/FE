01124cbf7e91749ca7dfbf7111450556df77ce2d
yerim1104 <101086604+yerim1104@users.noreply.github.com>
2022-05-04 17:25:09 +0900

    Login Signup MBTI 완료
    Login Signup MBTI 완료
    
    3rd page 진행중
    Nickname 완료
    Profile Image 진행중

--- src/pages/SignupMBTI.jsx
+++ src/pages/SignupMBTI.jsx
@@ -6,6 +6,7 @@ import { history } from '../redux/configStore'
 import styled from 'styled-components'
 import { StepBar } from '../components/StepBar'
 import Hi  from '../asset/MBTI_HI.svg'
+import Button from '../elements/Button';
 
 //Hook
 import { useForm} from "react-hook-form"
@@ -16,8 +17,7 @@ const SignupMBTI = () => {
   const [Mbti, setMbti] = useState({energy: null, insight: null, judgement: null, lifePattern: null});
 
   const {
-    register, handleSubmit, control,
-    formState: { errors } }
+    register, handleSubmit }
     = useForm();
 
   const onSubmit = async () => {
@@ -41,157 +41,162 @@ const SignupMBTI = () => {
   };
 
     return (
-      <form 
-      onSubmit={handleSubmit(onSubmit)}
-      >
-      <Container>
-     
-        <StepBar shape = "step2"/>
+      <form
+        onSubmit={handleSubmit(onSubmit)}>
+
+        <Container>
+          
+          <StepBar shape = "step2"/>
+          
+          <Hi/> //이모
         
-        <Hi />   
-        <TextContainer>
-          씨소 플레이그라운드에 처음 오신 회원님을 환영합니다!
-        </TextContainer>
+          <TextContainer>
+            <b>씨소 플레이그라운드</b>에 처음 오신 회원님을 환영합니다!
+          </TextContainer>
 
-          <OutlineContaierBar>
-              나만의 캐릭터 생성을 위한 간단한 몇가지 질문에 답해주세요.
-          </OutlineContaierBar>  
+            <OutlineContaierBar>
+                나만의 캐릭터 생성을 위한 간단한 몇가지 질문에 답해주세요.
+            </OutlineContaierBar>  
+              
+            <OutlineContainer>
 
-          <OutlineContainer>
-            
-              <SelectionContainer>
-                <div>
-                  <label>
-                    <FormCheckLeft
+              
+                  <QuestionText>
+                  <QuestionLabel/>
+                    <QuestionNum>Q1</QuestionNum>
+                    주말이 왔어요, 심심한 당신은 어떤 선택을 하나요?
+                  </QuestionText> 
+                  <div>
+                    <label>
+                      <FormCheckLeft
+                        {...register("energy")}
+                        type="radio"
+                        name="energy"
+                        onChange={changeRadio}
+                        value="E"
+                      />
+                      <FormCheckText >밖에 무조건 나간다.</FormCheckText>
+                    </label>
+                    <label>
+                      <FormCheckLeft
                       {...register("energy")}
-                      type="radio"
-                      name="energy"
-                      onChange={changeRadio}
-                      value="E"
-                    />
-                    <FormCheckText >밖에 무조건 나간다.</FormCheckText>
-                  </label>
-                  <label>
-                    <FormCheckLeft
-                    {...register("energy")}
-                      type="radio"
-                      name="energy"
-                      onChange={changeRadio}
-                      value="I"
-                    />
-                    <FormCheckText >집에 그냥 있는다.</FormCheckText>
-                  </label>
-                </div>
-                </SelectionContainer>
-            
-              <SelectionContainer>
-                <div>
-                  <label>
-                    <FormCheckLeft
-                      type="radio"
-                      name="insight"
-                      onChange={changeRadio}
-                      value="S"
-                    />
-                    <FormCheckText>밖에 무조건 나간다.</FormCheckText>
-                  </label>
-                  <label>
-                    <FormCheckLeft
-                      type="radio"
-                      name="insight"
-                      onChange={changeRadio}
-                      value="N"
-                    />
-                    <FormCheckText >집에 그냥 있는다.</FormCheckText>
-                  </label>
-                </div>
-              </SelectionContainer>
+                        type="radio"
+                        name="energy"
+                        onChange={changeRadio}
+                        value="I"
+                      />
+                      <FormCheckText >집에 그냥 있는다</FormCheckText>
+                    </label>
+                  </div>
 
-              <SelectionContainer>
-                <div>
-                  <label>
-                    <FormCheckLeft
-                      type="radio"
-                      name="judgement"
-                      onChange={changeRadio}
-                      value={"F"}
-                    />
-                    <FormCheckText >밖에 무조건 나간다.</FormCheckText>
-                  </label>
-                  <label>
-                    <FormCheckLeft
-                      type="radio"
-                      name="judgement"
-                      onChange={changeRadio}
-                      value={"T"}
-                    />
-                    <FormCheckText>집에 그냥 있는다.</FormCheckText>
-                  </label>
-                </div>
-              </SelectionContainer>
+                  <Line/>
+                  
+                  <QuestionText>
+                  <QuestionLabel/>
+                  <QuestionNum>Q2</QuestionNum>
+                    주말이 왔어요, 심심한 당신은 어떤 선택을 하나요?
+                  </QuestionText> 
 
-              <SelectionContainer>
-                <div>
-                  <label>
-                    <FormCheckLeft
-                      type="radio"
-                      name="lifePattern"
-                      onChange={changeRadio}
-                      value="J"
-                    />
-                    <FormCheckText >밖에 무조건 나간다.</FormCheckText>
-                  </label>
-                  <label>
-                    <FormCheckLeft
-                      type="radio"
-                      name="lifePattern"
-                      onChange={changeRadio}
-                      value="P"
-                    />
-                    <FormCheckText>집에 그냥 있는다.</FormCheckText>
-                  </label>
-                </div>
-              </SelectionContainer>
-      
-          </OutlineContainer>
-      
-        <Confirm type="submit" value="다음 단계"/>
-        </Container>
+                  <div>
+                    <label>
+                      <FormCheckLeft
+                        type="radio"
+                        name="insight"
+                        onChange={changeRadio}
+                        value="S"
+                      />
+                      <FormCheckText>밖에 무조건 나간다</FormCheckText>
+                    </label>
+                    <label>
+                      <FormCheckLeft
+                        type="radio"
+                        name="insight"
+                        onChange={changeRadio}
+                        value="N"
+                      />
+                      <FormCheckText >집에 그냥 있는다</FormCheckText>
+                    </label>
+                  </div>
+                
+                  <Line/>
+
+                  <QuestionText>
+                  <QuestionLabel/>
+                  <QuestionNum>Q3</QuestionNum>
+                    주말이 왔어요, 심심한 당신은 어떤 선택을 하나요?
+                  </QuestionText> 
+
+                  <div>
+                    <label>
+                      <FormCheckLeft
+                        type="radio"
+                        name="judgement"
+                        onChange={changeRadio}
+                        value={"F"}
+                      />
+                      <FormCheckText >밖에 무조건 나간다</FormCheckText>
+                    </label>
+                    <label>
+                      <FormCheckLeft
+                        type="radio"
+                        name="judgement"
+                        onChange={changeRadio}
+                        value={"T"}
+                      />
+                      <FormCheckText>집에 그냥 있는다</FormCheckText>
+                    </label>
+                  </div>
+                
+                  <Line/>
+
+                  <QuestionText>
+                    
+                    <QuestionLabel/>
+                    <QuestionNum>Q4</QuestionNum>
+
+                      주말이 왔어요, 심심한 당신은 어떤 선택을 하나요?
+                  </QuestionText> 
+
+                  <div>
+                    <label>
+                      <FormCheckLeft
+                        type="radio"
+                        name="lifePattern"
+                        onChange={changeRadio}
+                        value="J"
+                      />
+                      <FormCheckText >밖에 무조건 나간다</FormCheckText>
+                    </label>
+                    <label>
+                      <FormCheckLeft
+                        type="radio"
+                        name="lifePattern"
+                        onChange={changeRadio}
+                        value="P"
+                      />
+                      <FormCheckText>집에 그냥 있는다</FormCheckText>
+                    </label>
+                  </div>
+        
+            </OutlineContainer>
+          <MbtiConfirm type="submit"> 확인 </MbtiConfirm>
+          </Container>
         
         </form>
     )
 }
-const Confirm = styled.input`
-  margin: 2.5rem;
-  display: inline-block;
-  width: 23%;
-  height: 4rem;
-  background-color: #C1C1C1;
-  border-color: transparent;
-  border-radius: 0.75rem;
-  color: white;
-  font-weight: 500;
-  cursor: pointer;
-`
+
 const Container = styled.div`
    text-align: center;
-   width: 100%;
+   max-width: 100%;
 `;
 
 const TextContainer = styled.div`
   text-align: center;
-  margin: 1rem;
-  font-weight: bold;
+  margin: 0.1rem;
+  font-weight: 400;
   margin-bottom: 3rem;
-`;
-
-const OutlineContainer = styled.div`
-  margin: auto;
-  width: 50%;
-  height: 57%;
-  background: #FFFFFF;
-  box-shadow: 0px 4px 8px -4px rgba(22, 34, 51, 0.08), 0px 16px 24px rgba(22, 34, 51, 0.08);
-  border-radius: 0px 0px 28px 28px;
+  color:#242424;
 `;
 
 const OutlineContaierBar = styled.section`
@@ -205,23 +210,37 @@ const OutlineContaierBar = styled.section`
   background: #242424;
   box-shadow: 0px 4px 8px -4px rgba(22, 34, 51, 0.08), 0px 16px 24px rgba(22, 34, 51, 0.08);
   border-radius: 28px 28px 0px 0px ;
-`
+`;
 
-const SelectionContainer = styled.div`
-  width: 90%;
-  margin: 2rem;
-`
+const OutlineContainer = styled.div`
+  margin: auto;
+  width: 50%;
+  height: 57%;
+  background: #FFFFFF;
+  box-shadow: 0px 4px 8px -4px rgba(22, 34, 51, 0.08), 0px 16px 24px rgba(22, 34, 51, 0.08);
+  border-radius: 0px 0px 28px 28px;
+`;
+
+const QuestionText = styled.div`
+  position: relative;
+
+  display: flex;
+  color: #999999;
+  font-weight: 500;
+  margin: 2rem 5rem 0 5rem;
+
+`;
 
 const FormCheckText = styled.span`
-  margin: 1rem 0.5rem;
-  width: 45%;
+  margin: 1rem 0.5rem 2.5rem 0.5rem;
+  width: 40%;
   height: 4rem;
   line-height: 4rem;
   background-color: #edeff2;
   border-color: transparent;
   border-radius: 0.75rem;
   justify-content: center;
-  display: flex;
+  display: inline-block;
   align-items: center;
   cursor: pointer;
   color: #999999;
@@ -234,17 +253,52 @@ const FormCheckLeft = styled.input`
     background: none;
     padding: 0px 10px;
     text-align: center;
-    height: 35px;
+    height: 33px;
     line-height: 33px;
     font-weight: 500;
     display: none;
   }
   &:checked + ${FormCheckText} {
-    background: #e4794d;
-    color: #fff;
+    background: #fff;
+    color: #242424;
+    font-weight: bolder;
+    border: 3px solid #242424;
+    box-shadow: 0px 8px 16px -4px rgba(22, 34, 51, 0.08);
+
   }
   display: none;
 `;
 
+const QuestionLabel = styled.div`
+  position: absolute;
+  left: -8.27rem;
+  top: -1rem;
+  display: flex;
+  width: 3.19rem;
+  height: 3.31rem;
+  background-color: #ff4e4e;
+  border-radius: 0.5rem 0 0 0.5rem;
+  
+  box-shadow: 0rem 1rem 1.5rem rgba(22, 34, 51, 0.08);
+`
+const QuestionNum = styled.div`
+  color: white;
+  position: absolute;
+  left: -7.2rem;
+  top: -0.2rem;
+  display: flex;
+  font-weight: bolder;
+`
+
+const Line = styled.div`
+  position: relative;
+  width: 100%;
+  height: 0.3rem;
+  background: #F5F5F5;
+`
+
+const MbtiConfirm = styled(Button)`
+  
+`
 
 export default SignupMBTI
