2f5370275dbbac45704694c5bcb8b79d9bca5d17
saintrabby <saintrabby@gmail.com>
2022-05-17 13:40:21 +0900

    https
    https

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,7 +3,7 @@ import { cookies } from "../shared/cookie";
 
 const env = process.env.NODE_ENV;
 
-const targetServer = "https://sparta-ms.shop/"
+const targetServer = "https://sparta-ms.shop"
   // env === "http://3.38.104.97/"
 //env설정하기
 
