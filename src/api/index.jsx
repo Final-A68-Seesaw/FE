9492dd537baedb4d487a52ddea5367b833e7226e
saintrabby <saintrabby@gmail.com>
2022-05-26 04:15:07 +0900

    refresh
    refreshtoken

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -77,37 +77,31 @@ instance.interceptors.response.use(
     const originalReq = config; // 에러가 난 요청정보들
 
     if (status === 401 || status === 500) {
-      console.log('?', isTokenRefreshing);
       // 토큰이 없고,
       if (!isTokenRefreshing) {
         // 토큰 생성중이면, if 실행되지 않도록
         isTokenRefreshing = true; // false 일때는 true로 바꿔주고.
         const refreshToken = cookies.get("refreshToken"); // 리프레시 토큰을 쿠키에서 가져와서
 
-        console.log(refreshToken);
-
-        const { data } = await axios.post(`${targetServer}token`, {
+        const newAccessToken = await axios.post(`${targetServer}/user/refresh`, {
           refreshToken,
         });
 
-        console.log('data', data);
+        const newToken = (newAccessToken.headers.authorization.split(' ')[1])
 
-        const { accessToken: newAccessToken } = data;
-        cookies.set("accessToken", newAccessToken, {
+        cookies.set("accessToken", newToken, {
           path: "/",
-          maxAge: 604800, // 
+          maxAge: 86400, // 
         }); // 쿠키에 다시 저장
-
-        console.log('data', data);
 
         console.log("토큰 재생성 완료!"); // 토큰 생성이 완료되면
         isTokenRefreshing = false; // 토큰 생성중 상태를 fasle로 바꿔주고
 
         // 헤더를 새로운 액세스토큰으로 헤더 재설정
-        axios.defaults.headers.common.Authorization = `Bearer ${newAccessToken}`;
-        originalReq.headers.Authorization = `Bearer ${newAccessToken}`;
+        axios.defaults.headers.common.Authorization = `Bearer ${newToken}`;
+        originalReq.headers.Authorization = `Bearer ${newToken}`;
 
-        onTokenRefreshed(newAccessToken); // 첫 요청이 아닌 다른 쌓여있던 요청 다시 요청보내기
+        onTokenRefreshed(newToken); // 첫 요청이 아닌 다른 쌓여있던 요청 다시 요청보내기
         refreshSubscribers = []; // 요청 배열 초기화
         console.log("첫 요청도 다시 요청!");
         return axios(originalReq); // 첫 요청 다시 요청
