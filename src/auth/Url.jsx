7fb4ac7f2fe0343df6010f1183769ea2a783cdeb
saintrabby <saintrabby@gmail.com>
2022-05-26 21:06:58 +0900

    url

--- src/auth/Url.jsx
+++ src/auth/Url.jsx
@@ -1,6 +1,6 @@
 
 // const BASE_URL = process.env.REACT_APP_BASE_URL;
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+const REDIRECT_URI = 'https://play-seeso.com/user/kakao/callback';
 const CLIENT_KEY = '6f05e336898a8b021c45ac7c1f8770b8';
 
 const Url = {
