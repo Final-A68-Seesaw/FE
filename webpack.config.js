36d5de6ada88dec828d62e13f8bc7ee1ce487a80
yerim1104 <101086604+yerim1104@users.noreply.github.com>
2022-05-27 19:53:27 +0900

    OGTAG

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
             ],
           },
 
-          
+
         },
       },
     ]
@@ -99,7 +99,7 @@ module.exports = {
         'og:title': { property: 'og:title', content: '우리들의 플레이그라운드 SEESO!' },
         'og:type': { property: 'og:type', content: 'website' },
         'og:url': { property: 'og:url', content: 'https://play-seeso.com' },
-        'og:image': { property: 'og:image', content: '../src/asset/ogimage.png' },
+        'og:image': { property: 'og:image', content: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fl1fzW%2FbtrDiIPtqli%2FmrqSE5d7bttCPpnos8a35K%2Fimg.png' },
         'twitter:title': { name: 'twitter:title', content: '우리들의 플레이그라운드 SEESO' },
         'twitter:description': { name: 'twitter:description', content: '신조어도 배우고, 고민 해결책을 세대별로 얻어보세요!' },
         'twitter:image': { name: 'twitter:image', content: '../asset/ogimage.png' }
