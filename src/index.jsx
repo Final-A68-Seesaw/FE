9b4d7da2526fabe793191e6d69a59133197b4695
yerim1104 <101086604+yerim1104@users.noreply.github.com>
2022-06-05 00:59:32 +0900

    restore

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import * as React from "react";
 import ReactDom from "react-dom";
 import App from "./shared/App";
 import "./index.css";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 
 ReactDom.render(
   <BrowserRouter>
