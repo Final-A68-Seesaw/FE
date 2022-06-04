c8b7b70baf0497c807d62e7af43340ad56e0c2aa
yerim1104 <101086604+yerim1104@users.noreply.github.com>
2022-06-04 20:46:42 +0900

    hashrouter
    hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import * as React from "react";
 import ReactDom from "react-dom";
 import App from "./shared/App";
 import "./index.css";
 
 ReactDom.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.querySelector("#root")
 );
