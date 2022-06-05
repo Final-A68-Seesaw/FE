0d0a386fe5c8a696d628aaf7cec9048330b7aa7d
yerim1104 <101086604+yerim1104@users.noreply.github.com>
2022-06-05 15:37:38 +0900

    kakao url
    kakao url

--- src/auth/Url.jsx
+++ src/auth/Url.jsx
@@ -1,4 +1,4 @@
-const REDIRECT_URI = "http://play-seeso.com/user/kakao/callback";
+const REDIRECT_URI = "https://play-seeso.com/user/kakao/callback";
 const CLIENT_KEY = "6f05e336898a8b021c45ac7c1f8770b8";
 
 const Url = {
